011ff223c0215ebc52b20378d47dd2d4923bef86
cmbaker00 <cmbaker00@gmail.com>
2018-12-08 12:21:18 +1000

    Renamed some paramters with _ between words

--- skuas_DE.py
+++ skuas_DE.py
@@ -8,8 +8,8 @@ def model(y, t, params):
     r = y[0]
     pred1 = y[1]
     k = params.carrying_capacity_function(t, params)
-    drdt = params.preygrowthrate*r*(1-r/k) - .18*r*pred1
-    dp1dt = params.preygrowthrate*pred1*(1-pred1/(k*r))
+    drdt = params.prey_growth_rate*r*(1-r/k) - .18*r*pred1
+    dp1dt = params.prey_growth_rate*pred1*(1-pred1/(k*r))
     return [drdt, dp1dt]
 
 
@@ -24,7 +24,7 @@ def carrying_capacity(t, t_control, sigma, min_capacity, max_capacity):
 def carrying_capacity_params(t, params):
     return carrying_capacity(t, params.erad_time, params.sigma,
                                           params.min_carrying_capacity,
-                                          params.preycarryingcapacity)
+                                          params.prey_carrying_capacity)
 
 def plot_carrying_capacity(t_control, sigma, min_capacity, max_capacity, time_horison = 25):
     t_vals = np.arange(0, time_horison, 0.01)
@@ -39,10 +39,10 @@ if __name__ == '__main__':
     plot_carrying_capacity(t_control=2, sigma=.5, min_capacity=1, max_capacity=10)
 
     y0 = (15,10)
-    param_struct = namedtuple('Parameters', ['preygrowthrate', 'preycarryingcapacity',
+    param_struct = namedtuple('Parameters', ['prey_growth_rate', 'prey_carrying_capacity',
                                              'erad_time','min_carrying_capacity',
                                              'carrying_capacity_function','sigma'])
-    params = param_struct(preygrowthrate=1, preycarryingcapacity=15,
+    params = param_struct(prey_growth_rate=1, prey_carrying_capacity=15,
                           erad_time=2, min_carrying_capacity=y0[0]/2,
                           carrying_capacity_function=carrying_capacity_params, sigma = .5)
 
